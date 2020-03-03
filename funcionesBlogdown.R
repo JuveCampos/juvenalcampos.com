f97591db678f7071cdcc83280822794d48659ece
JuveCampos <juvenal.campos@cide.edu>
2020-03-03 16:08:35 -0600

    add Compatibilidad computadora trabajo

--- funcionesBlogdown.R
+++ funcionesBlogdown.R
@@ -6,14 +6,18 @@ blogdown::serve_site()
 
 # 3. Incluir nuevo post
 # Recuerda checar el addin si vas a publicar en RMarkdown
-blogdown::new_post(title = "Nuestro primer Post",
-                   author = "Juvenal Campos",
-                   date = Sys.Date()
-)
+# blogdown::new_post(title = "Nuestro primer Post",
+#                    author = "Juvenal Campos",
+#                    date = Sys.Date()
+# )
 
 # Temas en hugo> gohugo.io >> themes.gohugo.io
 
 # 4. Instalar tema
 blogdown::install_theme("lithium")
+
+# 5. Instalar Hugo
+# blogdown::install_hugo()
+
 
 # Las imagenes son relativas a la carpeta public. 
